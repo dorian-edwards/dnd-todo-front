65da66a0de86404d73182aebf3c20be200306271
Dorian Edwards <de.pharmd2018@gmail.com>
2022-08-18 21:17:20 -0400

    add aria landmarks

--- src/components/TitleBar.tsx
+++ src/components/TitleBar.tsx
@@ -1,7 +1,11 @@
 import DarkToggle from './DarkToggle'
 
 const TitleBar = () => (
-  <header role='banner' className=' flex justify-between mb-10 tablet:mb-12'>
+  <header
+    role='banner'
+    aria-roledescription='wrapper for title of application'
+    className=' flex justify-between mb-10 tablet:mb-12'
+  >
     <h1 className='text-white font-sans font-medium uppercase text-[40px] leading-[40px] tracking-[15px]'>
       todo
     </h1>
--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -110,7 +110,11 @@ const ToDo = () => {
   const completed = tasks.filter((task: TaskObject) => !task.completed).length
 
   return (
-    <div className='flex flex-col content-center w-[87%] max-w-[540px]'>
+    <div
+      className='flex flex-col content-center w-[87%] max-w-[540px]'
+      role='main'
+      aria-roledescription='wrapper for entire todo list app'
+    >
       <TitleBar />
       <TaskInput onChange={onChange} value={input} onSubmit={handleSubmit} />
       <DragDropContext onDragEnd={handleOnDragEnd}>
@@ -147,8 +151,6 @@ const ToDo = () => {
       />
     </div>
   )
-
-  // return <div>hello world</div>
 }
 
 export default ToDo
